f92772fab6f64dc0e30f0f920cf5f0267606c45e
jackwotherspoon <jackwoth@google.com>
2021-12-29 21:54:05 +0000

    add cloud run to terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,53 @@
 # Specify the GCP Provider
 provider "google" {
-    project = var.project_id
-    region  = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 # Create a GCS Bucket
 resource "google_storage_bucket" "image_bucket" {
-    name     = var.bucket_name
-    location = var.region
+  name     = var.bucket_name
+  location = var.region
+}
+
+# Enables the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+  disable_on_destroy = true
+}
+
+# Create a Cloud Run service
+resource "google_cloud_run_service" "image_gallery_service" {
+  name     = "image-gallery-service"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = var.container_image
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+  depends_on = [google_project_service.run_api]
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.image_gallery_service.location
+  project     = google_cloud_run_service.image_gallery_service.project
+  service     = google_cloud_run_service.image_gallery_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,20 @@
 variable "project_id" {
-    description = "Google Project ID."
-    type        = string
+  description = "Google Project ID."
+  type        = string
 }
 
 variable "bucket_name" {
-    description = "GCS Bucket name. Value must be unique."
-    type        = string
+  description = "GCS Bucket name. Value must be unique."
+  type        = string
 }
 
 variable "region" {
-    description = "Google Cloud region"
-    type        = string
-    default     = "us-central1"
+  description = "Google Cloud region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "container_image" {
+  description = "Google Container Registry image name"
+  type        = "string"
 }
